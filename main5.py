4d8e286d61194ebd5974976101451523a2449688
nemozx00-ship-it <nemozx00@gmail.com>
2026-04-26 14:53:34 +0700

    Add files via upload

---
+++ main5.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.write('hello')
+st.title('')
+pass = st.text.input(label: 'password: ', type='password')
+a=st.button('login')
+#tạo input copy đoạn văn bất kyf.
+#nhấn nút button, đếm số từ đoạn văn đã copy.
